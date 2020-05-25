42784c1c0b7de42eab7d1f4c0852d53f30a54695
lessanonymous <63210198+lessanonymous@users.noreply.github.com>
2020-05-25 22:23:45 +0200

    added last bonus requirement

--- script.js
+++ script.js
@@ -9,7 +9,6 @@ const createTaskElement = task => {
     li.querySelector('.delete').addEventListener('click', removeTaskButtonClickHandler);
     li.querySelector('.done').addEventListener('change', doneCheckboxChangeHandler);
     li.querySelector('.edit').addEventListener('click', editTaskButtonClickHandler);
-    console.log('createTaskElement');
     return li;
 };
 const addTaskElementsToDOM = async () => {
@@ -18,7 +17,6 @@ const addTaskElementsToDOM = async () => {
         let elements = [];
         data.forEach(task => elements.push(createTaskElement(task)));
         elements.forEach(element => todoList.appendChild(element));
-        console.log('addTaskElementsToDOM');
     }
 };
 // event handlers
@@ -35,13 +33,13 @@ const AddTaskButtonClickHandler = async e => {
         todoList.appendChild(createTaskElement(task));
     }
 };
-const removeTaskButtonClickHandler = async e => {
+const removeTaskButtonClickHandler = e => {
     const taskElement = e.target.parentElement;
     const id = taskElement.id;
-    await deleteData(id);
+    deleteData(id);
     todoList.removeChild(taskElement);
 };
-const doneCheckboxChangeHandler = async e => {
+const doneCheckboxChangeHandler = e => {
     const taskElement = e.target.parentElement;
     const id = taskElement.id;
     const description = taskElement.querySelector('.description');
@@ -49,7 +47,7 @@ const doneCheckboxChangeHandler = async e => {
         description: description.textContent,
         done: e.target.checked
     };
-    await putData(id, task);
+    putData(id, task);
     task.done ? description.classList.add('done') : description.classList.remove('done');
 };
 const editTaskButtonClickHandler = async e => {
@@ -69,25 +67,52 @@ const editTaskButtonClickHandler = async e => {
     taskElement.replaceChild(cancelButton, deleteButton);
     taskElement.replaceChild(descriptionInputField, description);
 };
-const cancelButtonClickHandler = e => {
+const cancelButtonClickHandler = async e => {
+    const data = await getData();
     const taskElement = e.target.parentElement;
+    const id = taskElement.id;
+    currentDescription = data.find(task => task.id === id).description;
     const saveButton = taskElement.querySelector('.save');
     const deleteButton = document.createElement('i');
     const descriptionInputField = taskElement.querySelector('.description');
     const description = document.createElement('span');
-    descriptionInputField.value = description.textContent;
-    descriptionInputField.className = 'description';
-    cancelButton.className = 'fas fa-window-close cancel';
-    cancelButton.addEventListener('click', cancelButtonClickHandler);
-    const saveButton = document.createElement('i');
-    saveButton.className = 'fas fa-save save';
-    saveButton.addEventListener('click', saveTaskButtonClickHandler);
-    taskElement.replaceChild(saveButton, e.target);
-    taskElement.replaceChild(cancelButton, deleteButton);
-    taskElement.replaceChild(descriptionInputField, description);
+    const checkbox = taskElement.querySelector('.done');
+    description.textContent = currentDescription;
+    description.className = `description ${checkbox.checked ? 'done' : ''}`;
+    deleteButton.className = 'fas fa-trash-alt delete';
+    deleteButton.addEventListener('click', removeTaskButtonClickHandler);
+    const editButton = document.createElement('i');
+    editButton.className = 'fas fa-pencil-alt edit';
+    editButton.addEventListener('click', editTaskButtonClickHandler);
+    taskElement.replaceChild(deleteButton, e.target);
+    taskElement.replaceChild(editButton, saveButton);
+    taskElement.replaceChild(description, descriptionInputField);
 };
 const saveTaskButtonClickHandler = e => {
-
+    const taskElement = e.target.parentElement;
+    const descriptionInputField = taskElement.querySelector('.description');
+    if (descriptionInputField.value) {
+        const id = taskElement.id;
+        const checkbox = taskElement.querySelector('.done');
+        const task = {
+            description: descriptionInputField.value,
+            done: checkbox.checked
+        };
+        putData(id, task);
+        const cancelButton = taskElement.querySelector('.cancel');
+        const deleteButton = document.createElement('i');
+        const description = document.createElement('span');
+        description.textContent = descriptionInputField.value;
+        description.className = `description ${checkbox.checked ? 'done' : ''}`;
+        deleteButton.className = 'fas fa-trash-alt delete';
+        deleteButton.addEventListener('click', removeTaskButtonClickHandler);
+        const editButton = document.createElement('i');
+        editButton.className = 'fas fa-pencil-alt edit';
+        editButton.addEventListener('click', editTaskButtonClickHandler);
+        taskElement.replaceChild(deleteButton, cancelButton);
+        taskElement.replaceChild(editButton, e.target);
+        taskElement.replaceChild(description, descriptionInputField);
+    }
 };
 // add event handlers
 addTaskButton.addEventListener('click', AddTaskButtonClickHandler);
